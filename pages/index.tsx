7f261705cb1d58130d8c886ca6188a32505be0ec
MuhammadjonErgashev1 <mmuhammadjonergashev1@gmail.com>
2026-01-28 13:39:49 +0900

    feat: publish layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import styles from "@/styles/Home.module.css";
-import { Box, Container, Stack } from "@mui/material";
-import { brown, green } from "@mui/material/colors";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 
 const Home: NextPage=() => {
   return (
     <>
-     <Container>
-        <Stack flexDirection={"column"}>
+     <Stack>
+        <Stack className="home-page">
           <Stack>
             <Stack className="container">Popular properties</Stack>
           </Stack>
@@ -20,7 +18,7 @@ const Home: NextPage=() => {
             <Stack className="container">Top Properties</Stack>
           </Stack>
         </Stack>
-       </Container> 
+       </Stack> 
     </>
   );
 }
