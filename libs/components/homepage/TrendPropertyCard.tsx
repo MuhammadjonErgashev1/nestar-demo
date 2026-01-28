d8b967eb3a206098780b7a3716927fbcbfa5fbc2
MuhammadjonErgashev1 <mmuhammadjonergashev1@gmail.com>
2026-01-28 19:00:46 +0900

    feat: publish homepage screen component

---
+++ libs/components/homepage/TrendPropertyCard.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Stack, Box, Divider, Typography, IconButton } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+
+const TrendPropertyCard = () => {
+  return (
+    <Stack className="trend-card-box">
+      <Box
+        className={"card-img"}
+        style={{
+          backgroundImage: `url("/img/banner/types/apartment.webp")`,
+        }}
+      >
+        <div className={"status"}>
+          <img src="/img/icons/electricity.svg" alt="" />
+          <span>top</span>
+        </div>
+
+        <div className={"price"}>$10</div>
+      </Box>
+
+      <Box className={"info"}>
+        <strong className={"title"}>Kingston Buildings</strong>
+        <p className={"desc"}>Good wills</p>
+        
+        <div className={"options"}>
+          <div>
+            <img src="/img/icons/bed.svg" alt="" />
+            <span>2 bed</span>
+          </div>
+          <div>
+            <img src="/img/icons/room.svg" alt="" />
+            <span>7 rooms</span>
+          </div>
+          <div>
+            <img src="/img/icons/expand.svg" alt="" />
+            <span>0 m2</span>
+          </div>
+        </div>
+
+        <Divider sx={{ mt: "15px", mb: "17px" }} />
+
+        <div className={"bott"}>
+          <p>rent</p>
+          <div className={"view-like-box"}>
+               <IconButton color="default">
+                  <RemoveRedEyeIcon />
+               </IconButton>
+              <Typography className="view-cnt">50</Typography>
+            <IconButton color="default">
+               <FavoriteIcon style={{color: "red"}} />
+            </IconButton>
+              <Typography className="view-cnt">250</Typography>
+          </div>
+        </div>
+      </Box>
+    </Stack>
+  );
+};
+
+export default TrendPropertyCard
